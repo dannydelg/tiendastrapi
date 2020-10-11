b1cc7d1869b7820f1c88cb736449b8ec9685c069
Danny Delgado <danny_gdo@hotmail.com>
2020-10-11 10:39:14 -0600

    Segundo intento

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -20,3 +20,5 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+postgres://equlwhjjmqkpdk:28a978bdbd7683822ccad4d6e1941c6ea30048c6a21be1fc51a43ff7c2c0ff79@ec2-54-160-120-28.compute-1.amazonaws.com:5432/de8qqh4onj61m
